9e0901980a73439490f6b35c3a8891aa6ea1e2d3
LuisLeandro94 <luisleandro.wd@gmail.com>
2022-11-18 01:12:00 +0000

    Initial Commit

---
+++ trackr/trackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackrApp.swift
+//  trackr
+//
+//  Created by Luis on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct trackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
